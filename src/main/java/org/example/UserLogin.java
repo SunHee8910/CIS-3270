e3e2a922914f25df2e460f74bdfb4e3eccc38fb7
Christopher R <crodriguez39@student.gsu.edu>
2024-10-30 22:39:27 -0400

    Initial userLogin class

---
+++ src/main/java/org/example/UserLogin.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class UserLogin {
+    class userLogin{
+        String userName;
+        String passWord;
+        int userCount;
+
+        static int UserTotal{
+            userCount = 0;
+            userCount ++;
+            // count the total usages of user class
+
+        }
+
+        userLogin(String userName,String passWord){
+            this.userName = userName;
+            this.passWord = passWord;
+        }
+
+
+    }
+
+    public static void main(String[]args){
+
+    }
+}
