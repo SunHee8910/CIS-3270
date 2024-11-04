73b44b19719b3cfded0414c02a26860aaec0639d
Christopher R <crodriguez39@student.gsu.edu>
2024-11-04 00:13:22 -0500

    Initial 7.4 lines

---
+++ src/main/java/org/example/FlightSearch.java
@@ -0,0 +1,25 @@
+
+
+public class FlightSearch {
+
+    String departureCity;
+    String arrivalCity;
+    String date;
+    String flightTime;
+
+    FlightSearch(){
+        this.departureCity = "Unknown";
+        this.arrivalCity = "Unknown";
+        this.date = "Unknown";
+        this.flightTime = "Unknown";
+    }
+
+
+
+
+    /*
+    The following is an initalization of the flight search system
+    if (loggedIn){
+
+     */
+}
