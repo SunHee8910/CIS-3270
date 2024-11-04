5fa5fd3df2ec9aa3f3b12c37a49e34160f97be60
ethan misere <emisere93@gmail.com>
2024-11-04 11:54:44 -0500

    Ethan 7.5 groundwork

---
+++ src/main/java/org/example/Duplicate7_5.java
@@ -0,0 +1,25 @@
+//import java.util.Date;
+//import java.util.List;
+//import java.util.ArrayList;
+//
+//public class conflict75 {
+//    public static void main(String[] args){
+//        Date newFlightdate;
+//        int newFlighthour;
+//        int newFlightminute;
+//
+//        List<Flight> flights = new ArrayList<Flight>(); // List of preexisting Flight class objects//
+//
+//        for (int i = 0; i < flights.length; i ++ ){ //Code will compare new flight with existing flights ensuring customers have at least 15 minutes in between flights//
+//            if (newFlightdate == flights[i].Flightdate){
+//
+//            }
+//        }
+//
+//
+//
+//
+//
+//
+//    }
+//}
