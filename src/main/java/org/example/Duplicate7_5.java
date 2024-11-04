7682370851ab4fb508bce4188a355adc91451d2f
ethan misere <emisere93@gmail.com>
2024-11-04 12:01:27 -0500

    Ethan 7.5 groundwork

--- src/main/java/org/example/Duplicate7_5.java
+++ src/main/java/org/example/Duplicate7_5.java
@@ -8,7 +8,7 @@
 //        int newFlighthour;
 //        int newFlightminute;
 //
-//        List<Flight> flights = new ArrayList<Flight>(); // List of preexisting Flight class objects//
+//        List<Flight> flights = new ArrayList<Flight>(); // List of preexisting Flight class objects
 //
 //        for (int i = 0; i < flights.length; i ++ ){ //Code will compare new flight with existing flights ensuring customers have at least 15 minutes in between flights//
 //            if (newFlightdate == flights[i].Flightdate){
@@ -18,8 +18,8 @@
 //
 //
 //
-//
-//
+// Eventually Will make method which returns true or false if there is a conflict
+//Need to import Flight list from 7.2 and flight class from 7.1
 //
 //    }
 //}
